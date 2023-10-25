5e7ea22642ea56ad267b4da5b61df79e439f8b4b
akdntmrkdjqtsp <gytjd9516@naver.com>
2023-10-25 19:46:17 +0900

    Initial commit

---
+++ src/main/java/com/example/metagachon/MetaGachonApplication.java
@@ -0,0 +1,13 @@
+package com.example.metagachon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaGachonApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(MetaGachonApplication.class, args);
+    }
+    
+}
---
+++ src/main/java/com/example/metagachon/Room.java
@@ -0,0 +1,2 @@
+package com.example.metagachon;public class Room {
+}
